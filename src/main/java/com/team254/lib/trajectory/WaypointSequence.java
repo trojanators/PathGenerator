d4a9696a23743824bc9ada630e209586887eddb8
NICKY B <nickblackburn02@gmail.com>
2020-10-01 09:56:09 -0400

    Add comments in WayPointSequence

--- src/main/java/com/team254/lib/trajectory/WaypointSequence.java
+++ src/main/java/com/team254/lib/trajectory/WaypointSequence.java
@@ -12,6 +12,7 @@ import org.usfirst.frc.team5740.Main;
  * @author Art Kalb
  * @author Stephen Pinkerton
  * @author Jared341
+ * Modified by @author Nicholas Blackburn
  */
 public class WaypointSequence {
 
@@ -37,16 +38,17 @@ public class WaypointSequence {
 
   static ArrayList<Waypoint> waypoints_ = new ArrayList<Waypoint>();
 
-
-  //TODO: Fix Incramenting
+  /***
+   * Created an new Way of Incrementing and waypoint Storage
+   * @param w
+   * @param WaypointID
+   */
   public void addWaypoint(Waypoint w, int WaypointID) {
       waypoints_.add(WaypointID, w);
       Main.logger.warning("Waypoints In list" + waypoints_.size());
       num_waypoints_ = WaypointID;
     
   }
-  
-  
 
   public int getNumWaypoints() {
     return num_waypoints_;
@@ -60,6 +62,7 @@ public class WaypointSequence {
     }
   }
   
+  //TODO: fix Invert Y
   /*
   public WaypointSequence invertY() {
     WaypointSequence inverted = new WaypointSequence(waypoints_.length);
