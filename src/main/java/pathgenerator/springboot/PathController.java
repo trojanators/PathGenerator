e173029f7d142d1431da102de9a75d8267aa149a
NICKY B <nickblackburn02@gmail.com>
2020-10-06 10:21:35 -0400

    added base semi layout for spring boot java backend

---
+++ src/main/java/pathgenerator/springboot/PathController.java
@@ -0,0 +1,24 @@
+package pathgenerator.springboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * this class is for controlling the Data passed Via Path html to java backend
+ * @author Nicholas Blackburn
+ */
+@Controller
+@RequestMapping("path")
+public class PathController {
+    
+    @GetMapping("pathCreator")
+    public String pathcreator(){
+        return "pathCreator";
+    }
+
+    @GetMapping("pathDisplay")
+    public String pathDisplay(){
+        return "pathDisplay";
+    }
+}
