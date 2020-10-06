b1a5fcd2834c6740c6a97c881f963b9cdde5749b
NICKY B <nickblackburn02@gmail.com>
2020-10-06 18:25:00 -0400

    added ui layout of data page "

---
+++ src/main/java/pathgenerator/util/CanvasHandler.java
@@ -0,0 +1,19 @@
+package pathgenerator.util;
+
+import java.util.ArrayList;
+
+import pathgenerator.trajectory.Path;
+
+public class CanvasHandler {
+
+
+    private static ArrayList<Double>nodeXArray = new ArrayList<>();
+    private static ArrayList<Double>nodeYArray = new ArrayList<>();
+
+
+    public void addFillArray(int index, Path path){
+        nodeXArray.add(index, path.getPair().left.getX());
+        nodeXArray.add(index, path.getPair().right.getX());
+    }
+    
+}
