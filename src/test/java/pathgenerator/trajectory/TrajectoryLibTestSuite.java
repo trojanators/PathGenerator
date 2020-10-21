4b4a1d20c1c3c32a59dc56aabe8bd16c49936d82
Seth Peasley <seth.peasley@gmail.com>
2020-10-21 11:34:50 -0400

    Keeping for historical reasons, but not needed for testing.

--- src/test/java/pathgenerator/trajectory/TrajectoryLibTestSuite.java
+++ src/test/java/pathgenerator/trajectory/TrajectoryLibTestSuite.java
@@ -1,39 +1,55 @@
 
-package pathgenerator.trajectory;
+/* I'm keeping this file for historical purposes, but I don't think it's required for the gradle build system. -SP*/
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
 
-import junit.runner.*;
-/**
- * Test suite.
- *
- * @author Jared341
- */
-@RunWith(Suite.class)
-@SelectPackages ({ pathgenerator.trajectory.TrajectoryGeneratorTest.class, pathgenerator.trajectory.SplineTest.class,
-    pathgenerator.trajectory.PathGeneratorTest.class,
-    pathgenerator.trajectory.io.SerializationDeserializationTest.class })
-public class TrajectoryLibTestSuite {
 
-  @BeforeAll
-  public static void setUpClass() throws Exception {
-  }
+// package pathgenerator.trajectory;
 
-  @AfterAll
-  public static void tearDownClass() throws Exception {
-  }
+// import org.junit.jupiter.api.AfterAll;
+// import org.junit.jupiter.api.AfterEach;
+// import org.junit.jupiter.api.BeforeAll;
+// import org.junit.jupiter.api.BeforeEach;
 
-  @BeforeEach
-  public void setUp() throws Exception {
-  }
+// import org.junit.jupiter.api.RunWith;
 
-  @AfterEach
-  public void tearDown() throws Exception {
-  }
+// import org.junit.runner.RunWith;
+// import org.junit.runners.Suite;
 
-}
+// import junit.runner.*;
+
+// /**
+//  * Test suite.
+//  *
+//  * @author Jared341
+//  */
+
+//  /* Team 254 code
+// @RunWith(Suite.class)
+// @SelectPackages ({ pathgenerator.trajectory.TrajectoryGeneratorTest.class, pathgenerator.trajectory.SplineTest.class,
+//     pathgenerator.trajectory.PathGeneratorTest.class,
+//     pathgenerator.trajectory.io.SerializationDeserializationTest.class })
+//     */
+
+// // Annotating a class with @RunWith(JUnitPlatform.class) allows it to be run with IDEs and build systems that support JUnit 4 but do not yet support the JUnit Platform directly. 
+// // https://junit.org/junit5/docs/5.0.0-M1/api/org/junit/platform/runner/JUnitPlatform.html
+// @RunWith(JUnitPlatform.class)
+
+// public class TrajectoryLibTestSuite {
+
+//   @BeforeAll
+//   public static void setUpClass() throws Exception {
+//   }
+
+//   @AfterAll
+//   public static void tearDownClass() throws Exception {
+//   }
+
+//   @BeforeEach
+//   public void setUp() throws Exception {
+//   }
+
+//   @AfterEach
+//   public void tearDown() throws Exception {
+//   }
+
+// }
