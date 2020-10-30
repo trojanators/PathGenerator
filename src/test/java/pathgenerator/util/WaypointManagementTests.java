43211c4b594c0f016fbb7ab7a32c877cba7c19f7
Nicholas Blackburn <nickblackburn02@gmail.com>
2020-10-30 10:09:48 -0400

    added More tests to waypoint management

--- src/test/java/pathgenerator/util/WaypointManagementTests.java
+++ src/test/java/pathgenerator/util/WaypointManagementTests.java
@@ -5,19 +5,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import pathgenerator.util.ChezyMath;
 import pathgenerator.util.WaypointManagement;
-import pathgenerator.util.WaypointManagement;
 
+/***
+ * This Test class is here to set the waypoint management Functionality 
+ * @author Nicholas Blackburn
+ * @author Seth Peasly
+ */
 public class WaypointManagementTests 
 {
 
     @Test
-    public void Create_Waypoint_With_Something_Creates_1_Element_Sequence()
+    public void Create_A_Single_Element_Sequence()
     {
-        //Arrange, Act, Assert
-
         // Arrange
-        // (final WaypointTableData data, Boolean enableRando, Boolean enablePiCalc, Boolean enableNegPi, final double wheebase, String pathName, String Location, Boolean genpath, int Seqnum) {
-        //     int waypointId = data.getId();
         var tableData = new WaypointTableData(0, 2.5, 2.0, 0.5, 2.4, 0.1, 1.0, 0.25);
         
         Boolean rando = true;
@@ -32,9 +32,10 @@ public class WaypointManagementTests
 
         // Act
         WaypointManagement wypMgt = new WaypointManagement();
-        wypMgt.createWaypoint(tableData, rando, piCalc, negPi, whelbs, pathName, loc, genPath, sqnm);
         var sequence = wypMgt.getWaypointSequence(); 
-
+        sequence.clearWaypointsSequence();
+        wypMgt.createWaypoint(tableData, rando, piCalc, negPi, whelbs, pathName, loc, genPath, sqnm);
+        
         // Assert
         //assert(expected, actual)
         var somePt = sequence.getWaypoint(0);
@@ -44,6 +45,78 @@ public class WaypointManagementTests
         assertEquals(1, sequence.getNumWaypoints());
 
     }
+
+    @Test
+    public void Create_Waypoint_With_A_Negitive_PI_Theta_Sequence()
+    {
+        //Arrange, Act, Assert
+
+        // Arrange
+        var tableData = new WaypointTableData(0, 3.5, 4.0, 5.5, 6.4, 7.1, 8.0, 9.25);
+        
+        Boolean rando = true;
+        Boolean piCalc = false;
+        Boolean negPi = true;
+        
+        double whelbs = 120.0;
+        String pathName = "./here";
+        String loc = "./there";
+        Boolean genPath = false;
+        int sqnm = 1;
+
+        // Act
+        WaypointManagement wypMgt = new WaypointManagement();
+        var sequence = wypMgt.getWaypointSequence(); 
+        sequence.clearWaypointsSequence();
+        wypMgt.createWaypoint(tableData, rando, piCalc, negPi, whelbs, pathName, loc, genPath, sqnm);
+
+        // Assert
+        //assert(expected, actual)
+        var somePt = sequence.getWaypoint(0);
+        assertEquals(3.5/12, somePt.getX());
+        assertEquals(4.0/12, somePt.getY());
+        assertEquals(- Math.PI /5.5, somePt.getTheta());
+        assertEquals(1, sequence.getNumWaypoints());
+
+    }
+
+
+    @Test
+    public void Create_Waypoint_With_A_Postive_PI_Theta_Sequence()
+    {
+        //Arrange, Act, Assert
+
+        // Arrange' sall the data that is needed 
+        var tableData = new WaypointTableData(0, 10.5, 11.0, 12.5, 13.4, 14.1, 15.0, 16.25);
+        
+        Boolean rando = true;
+        Boolean piCalc = true;
+        Boolean negPi = false;
+        
+        double whelbs = 120.0;
+        String pathName = "./here";
+        String loc = "./there";
+        Boolean genPath = false;
+        int sqnm = 1;
+
+        // Act's up on the Data 
+        WaypointManagement wypMgt = new WaypointManagement();
+        var sequence = wypMgt.getWaypointSequence(); 
+        sequence.clearWaypointsSequence();
+        wypMgt.createWaypoint(tableData, rando, piCalc, negPi, whelbs, pathName, loc, genPath, sqnm);
+
+        // Assert checks the data 
+        //assert(expected, actual)
+        var somePt = sequence.getWaypoint(0);
+        assertEquals(10.5/12, somePt.getX());
+        assertEquals(11.0/12, somePt.getY());
+        assertEquals(Math.PI /12.5, somePt.getTheta());
+        assertEquals(1, sequence.getNumWaypoints());
+
+    }
+
+
+
     
     /*
 
