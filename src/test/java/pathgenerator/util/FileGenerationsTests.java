39dccb7d6c95a276720a6c4fbff418516d2bb178
Nicholas Blackburn <nickblackburn02@gmail.com>
2020-10-31 10:23:03 -0400

    created filegeneratorTests class

---
+++ src/test/java/pathgenerator/util/FileGenerationsTests.java
@@ -0,0 +1,27 @@
+package pathgenerator.util;
+
+import com.amihaiemil.eoyaml.YamlStream;
+
+import org.junit.Test;
+import org.simpleyaml.configuration.file.YamlFile;
+import org.yaml.snakeyaml.Yaml;
+
+import pathgenerator.trajectory.Path;
+import pathgenerator.trajectory.PathGenerator;
+import pathgenerator.trajectory.WaypointSequence;
+
+/**
+ * This Tests the Functionality fot the FileGeneration
+ * 
+ * @author Nicholas Blackburn
+ */
+//TODO: acutally think file generator output testing 
+public class FileGenerationsTests {
+    
+
+    @Test
+    public void  Checks_Yaml_File_Gen_Data(){
+        //Arrange
+        
+    }
+}
